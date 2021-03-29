1fd435ced2962b5cd87ab0ff6d10525ea285836f
Nathan Waddell <nathan.waddell@gatewayapps.com>
2021-03-29 14:58:10 -0500

    creating dataset from csv file and printing data

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -9,3 +9,10 @@ Created on Mon Mar 29 14:23:22 2021
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Importing datasets
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+print(x)
+print(y)
