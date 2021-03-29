b4720bb7b7b5596ec583540cd6ffad9e770d7c26
Nathan Waddell <nathan.waddell@gatewayapps.com>
2021-03-29 15:45:54 -0500

    Splitting dataset into training / test sets

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -14,15 +14,12 @@ import pandas as pd
 dataset = pd.read_csv('Data.csv')
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
-print(x)
-print(y)
 
 # Take care of missing data
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
-print(x)
 
 # Encoding categorical data
 
@@ -31,10 +28,12 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
 x = np.array(ct.fit_transform(x))
-print(x)
 
 # Encoding the dependent variable
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 y = le.fit_transform(y)
-print(y)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1)
