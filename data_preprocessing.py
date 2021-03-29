dd808ec4995dab006a3bae3495f2484944b4add1
Nathan Waddell <nathan.waddell@gatewayapps.com>
2021-03-29 15:14:58 -0500

    Fixing missing data in csv using mean

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -16,3 +16,10 @@ x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 print(x)
 print(y)
+
+# Take care of missing data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+print(x)
