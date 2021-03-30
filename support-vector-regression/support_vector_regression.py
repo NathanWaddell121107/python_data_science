9bda3c83952a3d9ce9c2b04d55871020fb54b76d
Nathan Waddell <nathan.waddell@gatewayapps.com>
2021-03-30 14:02:44 -0500

    Adding file struct for support-vector-regression

---
+++ support-vector-regression/support_vector_regression.py
@@ -0,0 +1,11 @@
+# Support Vector Regression (SVR)
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
