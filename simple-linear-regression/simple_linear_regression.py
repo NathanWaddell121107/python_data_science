c1d0f90197395ae9619e194d56b0ec10080f6aeb
Nathan Waddell <nathan.waddell@gatewayapps.com>
2021-03-30 07:42:50 -0500

    Adding a visualizer for training set data

--- simple-linear-regression/simple_linear_regression.py
+++ simple-linear-regression/simple_linear_regression.py
@@ -21,3 +21,13 @@ regressor.fit(x_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(x_test)
+
+# Visualize the Training set results
+plt.scatter(x_train, y_train, color = 'red')
+plt.plot(x_train, regressor.predict(x_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualize the Test set results
