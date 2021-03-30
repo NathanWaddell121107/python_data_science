296364fa296daa75f534b4971a81f1676deb3ee3
Nathan Waddell <nathan.waddell@gatewayapps.com>
2021-03-30 13:18:58 -0500

    Adding training for the poly regression

--- polynomial-regression/polynomial_regression.py
+++ polynomial-regression/polynomial_regression.py
@@ -7,15 +7,20 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
-x = dataset.iloc[:, 1:-1].values
+X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
 # Training the Linear Regression model on the whole dataset
 from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
-lin_reg.fit(x, y)
+lin_reg.fit(X, y)
 
 # Training the Polynomial Regression model on the whole dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 2)
+X_poly = poly_reg.fit_transform(X)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
 
 # Visualizing the Linear Regression results
 
