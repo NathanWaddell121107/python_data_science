0dc6a8cf140eeadd8c14c789ffc7bea69a923afd
Nathan Waddell <nathan.waddell@gatewayapps.com>
2021-03-30 13:39:36 -0500

    Adding visualization for linear regression results

--- polynomial-regression/polynomial_regression.py
+++ polynomial-regression/polynomial_regression.py
@@ -17,12 +17,18 @@ lin_reg.fit(X, y)
 
 # Training the Polynomial Regression model on the whole dataset
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 2)
+poly_reg = PolynomialFeatures(degree = 4)
 X_poly = poly_reg.fit_transform(X)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
 
 # Visualizing the Linear Regression results
+plt.scatter(X, y, color = 'Red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
 
 # Visualizing the Polynomial Regression results
 
