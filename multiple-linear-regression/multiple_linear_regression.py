4d16b3cd614984ff813cbf75772469420ac62195
Nathan Waddell <nathan.waddell@gatewayapps.com>
2021-03-30 10:05:31 -0500

    Encoding the categorical data and splitting data into training / test data

--- multiple-linear-regression/multiple_linear_regression.py
+++ multiple-linear-regression/multiple_linear_regression.py
@@ -9,3 +9,13 @@ import pandas as pd
 dataset = pd.read_csv('50_Startups.csv')
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
+
+# Encode the categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers = [('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+x = np.array(ct.fit_transform(x))
+
+# Split into training / test sets
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1)
