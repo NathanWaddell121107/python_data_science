57923ce9b569ebc764147eca1ae8c8fa5b4da8d5
Nathan Waddell <nathan.waddell@gatewayapps.com>
2021-03-30 07:23:31 -0500

    Training simple linear regression model on training set

--- simple-linear-regression/simple_linear_regression.py
+++ simple-linear-regression/simple_linear_regression.py
@@ -7,5 +7,14 @@ import matplotlib.pyplot as plt
 
 # Import the dataset
 dataset = pd.read_csv('Salary_Data.csv')
-x = dataset.iloc(:, :-1).values
-y = dataset.iloc(:, -1).values
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Split into training / test sets
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1)
+
+# Training the simple linear regression model on training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
