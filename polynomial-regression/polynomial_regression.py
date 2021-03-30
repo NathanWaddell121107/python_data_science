feb9aea9596100ba14aea45e15ae8e744e186215
Nathan Waddell <nathan.waddell@gatewayapps.com>
2021-03-30 13:10:07 -0500

    training the linear regression model on whole dataset

--- polynomial-regression/polynomial_regression.py
+++ polynomial-regression/polynomial_regression.py
@@ -7,5 +7,22 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:-1].values
+x = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
+
+# Training the Linear Regression model on the whole dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(x, y)
+
+# Training the Polynomial Regression model on the whole dataset
+
+# Visualizing the Linear Regression results
+
+# Visualizing the Polynomial Regression results
+
+# Visualizing the Polynomial Regression results (for higher resolution and smoother curve)
+
+# Predicting a new result with Linear Regression
+
+# Predicting a new result with Polynomial Regression
