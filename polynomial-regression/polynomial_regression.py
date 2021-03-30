03e5228022ce06ca428ae5777444f26f9094cd6c
Nathan Waddell <nathan.waddell@gatewayapps.com>
2021-03-30 12:23:43 -0500

    Creating file struct for polynomial regression

---
+++ polynomial-regression/polynomial_regression.py
@@ -0,0 +1,11 @@
+# Polynomial Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
